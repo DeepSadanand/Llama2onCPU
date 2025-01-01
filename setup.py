b662cc11a5afa6865a6d8231c8a5384fd7a12082
Sadanand Howal <howal.sada@gmail.com>
2025-01-01 11:19:37 +0530

    setup.py updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version = "0.0.0",
+    author = "Sadanand",
+    author_email = "sadanand@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
